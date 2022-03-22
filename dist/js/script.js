e410d91851360413fdecfe45cffce5429f969c9d
Brendon Martin <brendonmartin657@gmail.com>
2022-03-22 05:17:21 -0400

    Improved on preventing scrolling with Mobile Menu open

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,17 +5,39 @@ const doc = document,
   navToggle = document.getElementById("js-nav-toggle"),
   navMenu = document.getElementById("js-sidebar");
 
+const toggleBodyFreeze = () => {
+  const { style, classList } = body;
+
+  if (classList.contains("js-freeze")) {
+    // we use the location we captured and turn it into a positive integar
+    // so that it can be used in the scrollTo() function
+    const scrollOffset = parseInt(style.top) * -1;
+
+    classList.remove("js-freeze");
+    window.scrollTo(0, scrollOffset);
+    style.top = "";
+  }
+  else {
+    // we capture the user's current location on page
+    style.top = `-${window.pageYOffset}px`;
+    classList.add("js-freeze");
+  }
+
+};
+
 navToggle.addEventListener("click", (event) => {
   navToggle.classList.toggle("active"),
     navMenu.classList.toggle("active"),
     navMenu.classList.add("js-mobile"),
-    body.classList.toggle("js-freeze");
+    toggleBodyFreeze();
+
+
 
 
   function removeMobileClass() {
     navMenu.classList.remove("js-mobile");
     window.removeEventListener("resize", removeMobileClass);
-  }
+  };
 
   window.addEventListener("resize", removeMobileClass);
 });
