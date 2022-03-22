fb5d166b0cdbc50598e7b7ab34db11d16f0d2afe
Brendon Martin <brendonmartin657@gmail.com>
2022-03-22 03:20:30 -0400

    Fixed window transition issue and preventing window from scrolling on desktop

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,21 @@
-const navToggle = document.getElementById("js-nav-toggle"),
+// Need to add class to body to prevent scrolling while the menu is open
+
+const doc = document,
+  body = doc.body,
+  navToggle = document.getElementById("js-nav-toggle"),
   navMenu = document.getElementById("js-sidebar");
 
 navToggle.addEventListener("click", (event) => {
   navToggle.classList.toggle("active"),
-    navMenu.classList.toggle("active");
+    navMenu.classList.toggle("active"),
+    navMenu.classList.add("js-mobile"),
+    body.classList.toggle("js-freeze");
+
+
+  function removeMobileClass() {
+    navMenu.classList.remove("js-mobile");
+    window.removeEventListener("resize", removeMobileClass);
+  }
+
+  window.addEventListener("resize", removeMobileClass);
 });
