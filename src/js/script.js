36961b8ffa21694f5547d7712a2680e05de6b8c3
Brendon Martin <brendonmartin657@gmail.com>
2022-03-30 09:53:31 -0400

    Added progressive enhancement to sidebar

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,43 @@
-"use strict";
-
 // Need to add class to body to prevent scrolling while the menu is open
-var doc = document,
+
+const doc = document,
   body = doc.body,
   navToggle = document.getElementById("js-nav-toggle"),
   navMenu = document.getElementById("js-sidebar");
 
-var toggleBodyFreeze = function toggleBodyFreeze() {
-  var style = body.style,
-    classList = body.classList;
+const toggleBodyFreeze = () => {
+  const { style, classList } = body;
 
   if (classList.contains("js-freeze")) {
     // we use the location we captured and turn it into a positive integar
     // so that it can be used in the scrollTo() function
-    var scrollOffset = parseInt(style.top) * -1;
+    const scrollOffset = parseInt(style.top) * -1;
+
     classList.remove("js-freeze");
     window.scrollTo(0, scrollOffset);
     style.top = "";
-  } else {
+  }
+  else {
     // we capture the user's current location on page
-    style.top = "-".concat(window.pageYOffset, "px");
+    style.top = `-${window.pageYOffset}px`;
     classList.add("js-freeze");
   }
+
 };
 
-navToggle.addEventListener("click", function (event) {
+navToggle.addEventListener("click", (event) => {
   navToggle.classList.toggle("active"),
     navMenu.classList.toggle("active"),
     navMenu.classList.add("js-mobile"),
     toggleBodyFreeze();
 
+
+
+
   function removeMobileClass() {
     navMenu.classList.remove("js-mobile");
     window.removeEventListener("resize", removeMobileClass);
-  }
+  };
 
   window.addEventListener("resize", removeMobileClass);
 });
